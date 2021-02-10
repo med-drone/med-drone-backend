08a8f4b4b04af29b0574f56968ac345996db62da
d-e-k-k <davideberhart8@gmail.com>
2021-02-09 23:21:22 -0500

    Added register user route with hashed passwords

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
 
 router.get('/', (req, res, next) => {
 	res.send('hello');
@@ -12,10 +13,42 @@ router.get('/', (req, res, next) => {
 	//     .catch(next);
 });
 
-router.post('/register', (req, res, next) => {
-	const userData = req.body;
-	User.create(userData)
-		.then((user) => res.status(201).json(user))
+router.post('/register', async (req, res, next) => {
+	try {
+		const salt = await bcrypt.genSalt();
+		const hashedPassword = await bcrypt.hash(req.body.password, salt);
+		console.log(salt);
+		console.log(hashedPassword);
+
+		User.create({ ...req.body, password: hashedPassword }).then((user) => {
+			res.status(201).json(user);
+		});
+	} catch {
+		res.status(500);
+	}
+});
+
+router.post('/login', (req, res, next) => {
+	User.find({})
+		.then((users) => {
+			return users.find((user) => user.email === req.body.email);
+		})
+		.then(async (user) => {
+			if (user) {
+				if (await bcrypt.compare(req.body.password, user.password)) {
+					// Attribution for user.toJSON() to Nipek from Stack Overflow @ https://stackoverflow.com/questions/52781477/expected-payload-to-be-a-plain-object-mean. This changes the plan text to json so it can then be used by jwt and resturned as json in a later line.
+					const accessToken = jwt.sign(
+						user.toJSON(),
+						process.env.ACCESS_TOKEN_SECRET
+					);
+					res.status(200).json({ accessToken: accessToken, id: user._id });
+				} else {
+					res.status(401).send('Incorrect Username or Password');
+				}
+			} else {
+				res.status(401).send('Incorrect Username or Password');
+			}
+		})
 		.catch(next);
 });
 
