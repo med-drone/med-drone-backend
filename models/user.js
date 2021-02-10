d038af36b4b6c47780544f086054605bfb45dd63
d-e-k-k <davideberhart8@gmail.com>
2021-02-09 21:50:46 -0500

    Created user model

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('../db/connections');
+
+const UserSchema = new mongoose.Schema(
+	{
+		firstName: {
+			type: String,
+			required: true,
+		},
+		lastName: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+		},
+		loaction: {
+			type: String,
+			required: false,
+		},
+		doctorsNumber: {
+			type: String,
+			required: false,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+const User = mongoose.model('User', UserSchema);
+module.exports = Todo;
